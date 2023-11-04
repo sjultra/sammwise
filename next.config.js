f04878c70c47cba7f8e9aac7a1bb351b4e3782ab
Eduard Tamsa <eduard.ioan.tamsa@gmail.com>
2023-11-04 02:44:27 +0200

    Remove default value in values.yaml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  images: {
+    path: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  },
 
   publicRuntimeConfig: {
     webSocketBasePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
