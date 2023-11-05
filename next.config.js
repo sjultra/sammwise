4222bc77ee8620c42aa296a0e2743263a2fa52d9
Eduard Tamsa <eduard.ioan.tamsa@gmail.com>
2023-11-06 01:48:20 +0200

    Add hostname generic

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  hostname: '0.0.0.0',
   // images: {
   //   path: process.env.NEXT_PUBLIC_BASE_PATH || '',
   // },
