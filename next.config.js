ba096629225ee461b90ed470e869e1f3b8850342
Eduard Tamsa <eduard.ioan.tamsa@gmail.com>
2023-11-06 03:30:44 +0200

    Test new config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,67 @@
 module.exports = {
   basePath: process.env.NEXT_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_BASE_PATH || '',
+
   // assetPrefix: process.env.RANCHER_SERVER || '',
-  hostname: process.env.RANCHER_SERVER,
+  // hostname: process.env.RANCHER_SERVER,
   // images: {
   //   path: process.env.NEXT_PUBLIC_BASE_PATH || '',
   // },
 
-  publicRuntimeConfig: {
-    hmrEndpoint: 'wss://'+process.env.RANCHER_SERVER+process.env.NEXT_PUBLIC_BASE_PATH,
-    webSocketBasePath: 'wss://'+process.env.RANCHER_SERVER+process.env.NEXT_PUBLIC_BASE_PATH,
+  // publicRuntimeConfig: {
+  //   hmrEndpoint: 'wss://'+process.env.RANCHER_SERVER+process.env.NEXT_PUBLIC_BASE_PATH,
+  //   webSocketBasePath: 'wss://'+process.env.RANCHER_SERVER+process.env.NEXT_PUBLIC_BASE_PATH,
+  // }
+
+
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: process.env.NEXT_BASE_PATH // Proxy to Backend
+      },
+      {
+        has: [
+          {
+            type: 'host',
+            value: 'rancher.vzxy.net',
+          },
+        ],
+        source: '/',
+        destination: process.env.NEXT_BASE_PATH // Proxy to Backend
+      },
+      {
+        has: [
+          {
+            type: 'host',
+            value: 'rancher.vzxy.net',
+          },
+        ],
+        source: '/about',
+        destination: process.env.NEXT_BASE_PATH + '/about' // Proxy to Backend
+      },
+      {
+        has: [
+          {
+            type: 'host',
+            value: 'rancher.vzxy.net',
+          },
+        ],
+        source: '/assessment',
+        destination: process.env.NEXT_BASE_PATH + '/assessment' // Proxy to Backend
+      },
+      {
+        has: [
+          {
+            type: 'host',
+            value: 'rancher.vzxy.net',
+          },
+        ],
+        source: '/results',
+        destination: process.env.NEXT_BASE_PATH + '/results' // Proxy to Backend
+      }
+    ]
   }
-};
+}
+
+
