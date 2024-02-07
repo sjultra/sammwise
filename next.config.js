62fa813f67ba3e823b0db48d815f95403c7fc52b
Saturn-V <alexaaronpena@gmail.com>
2024-02-07 10:55:37 -0800

    test rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  basePath: process.env.NEXT_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_BASE_PATH || '',
+  // basePath: '/k8s/clusters/c-m-6wzgb6p6/api/v1/namespaces/sammwise/services/http:sammwise:80/proxy' || process.env.NEXT_BASE_PATH || '',
+  // assetPrefix: process.env.NEXT_BASE_PATH || '',
 
   // assetPrefix: process.env.RANCHER_SERVER || '',
   // hostname: process.env.RANCHER_SERVER,
@@ -14,54 +14,25 @@ module.exports = {
   // }
 
 
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: process.env.RANCHER_SERVER+process.env.NEXT_BASE_PATH // Proxy to Backend
-  //     },
-  //     {
-  //       has: [
-  //         {
-  //           type: 'host',
-  //           value: process.env.RANCHER_SERVER,
-  //         },
-  //       ],
-  //       source: '/',
-  //       destination: process.env.RANCHER_SERVER+process.env.NEXT_BASE_PATH // Proxy to Backend
-  //     },
-  //     {
-  //       has: [
-  //         {
-  //           type: 'host',
-  //           value: process.env.RANCHER_SERVER,
-  //         },
-  //       ],
-  //       source: '/about',
-  //       destination: process.env.RANCHER_SERVER+process.env.NEXT_BASE_PATH + '/about' // Proxy to Backend
-  //     },
-  //     {
-  //       has: [
-  //         {
-  //           type: 'host',
-  //           value: process.env.RANCHER_SERVER,
-  //         },
-  //       ],
-  //       source: '/assessment',
-  //       destination: process.env.RANCHER_SERVER+process.env.NEXT_BASE_PATH + '/assessment' // Proxy to Backend
-  //     },
-  //     {
-  //       has: [
-  //         {
-  //           type: 'host',
-  //           value: process.env.RANCHER_SERVER,
-  //         },
-  //       ],
-  //       source: '/results',
-  //       destination: process.env.RANCHER_SERVER+process.env.NEXT_BASE_PATH + '/results' // Proxy to Backend
-  //     }
-  //   ]
-  // }
+  async rewrites() {
+    return [
+      // {
+      //   source: '/',
+      //   destination: process.env.RANCHER_SERVER+process.env.NEXT_BASE_PATH // Proxy to Backend
+      // },
+      {
+        has: [
+          {
+            type: 'host',
+            value: 'https://rancher.vzxy.net' || process.env.RANCHER_SERVER,
+          },
+        ],
+        source: '/:path*',
+        destination: '/k8s/clusters/c-m-6wzgb6p6/api/v1/namespaces/sammwise/services/http:sammwise:80/proxy/:path*' || process.env.RANCHER_SERVER,
+        // basePath: false,
+      },
+    ]
+  }
 }
 
 
