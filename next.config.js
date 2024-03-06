24a504dbcea68458337db011865a7139bd399f01
Saturn-V <alexaaronpena@gmail.com>
2024-03-06 13:18:22 -0800

    remove nextjs config for rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // basePath: '/k8s/clusters/c-m-6wzgb6p6/api/v1/namespaces/sammwise/services/http:sammwise:80/proxy' || process.env.NEXT_BASE_PATH || '',
-  assetPrefix: 'https://rancher.vzxy.net/k8s/clusters/c-m-6wzgb6p6/api/v1/namespaces/sammwise/services/http:sammwise:80/proxy',
+  // assetPrefix: process.env.NEXT_BASE_PATH || '',
 
   // assetPrefix: process.env.RANCHER_SERVER || '',
   // hostname: process.env.RANCHER_SERVER,
@@ -25,7 +25,7 @@ module.exports = {
   //       ],
   //       // source: '/:path*',
   //       source: '/',
-  //       destination: '/k8s/clusters/c-m-6wzgb6p6/api/v1/namespaces/sammwise/services/http%3Asammwise%3A80/proxy/about',
+  //       destination: '/k8s/clusters/c-m-6wzgb6p6/api/v1/namespaces/sammwise/services/http%3Asammwise%3A80/proxy/',
   //     },
   //     {
   //       has: [
@@ -54,11 +54,9 @@ module.exports = {
   //           value: 'rancher.vzxy.net',
   //         },
   //       ],
-  //       source: '/results',
-  //       destination: '/k8s/clusters/c-m-6wzgb6p6/api/v1/namespaces/sammwise/services/http%3Asammwise%3A80/proxy/results',
+  //       source: '/assessment',
+  //       destination: '/k8s/clusters/c-m-6wzgb6p6/api/v1/namespaces/sammwise/services/http%3Asammwise%3A80/proxy/assessment',
   //     },
   //   ]
   // }
 }
-
-
